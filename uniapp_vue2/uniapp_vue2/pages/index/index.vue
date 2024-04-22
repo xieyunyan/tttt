6b84dbc9d22b900921a46a1ae28e58c5575ad9cb
595816021@qq.com <123456.com>
2024-04-22 19:39:51 +0800

    谢云燕上传

---
+++ uniapp_vue2/uniapp_vue2/pages/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="content">
+		welcome使用uniapp
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+.content{
+	text-align: center;
+	font-weight: bold;
+	font-size: 32px;
+	line-height: 100px;
+}
+</style>
